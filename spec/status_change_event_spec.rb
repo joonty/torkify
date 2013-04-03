0eada770081386378cc1e2f3ac1ebd74216e3fa1
Jon Cairns <jon@joncairns.com>
2013-04-03 15:21:18 +0100

    Add extra case to status change event spec

--- spec/status_change_event_spec.rb
+++ spec/status_change_event_spec.rb
@@ -18,5 +18,20 @@ module Torkify
       its(:file)  { should == @file }
       its(:event) { should == @inner_event }
     end
+
+    context "with a fail_now_pass event" do
+      before do
+        @type = 'fail_now_pass'
+        @file = 'file'
+        @inner_event = PassOrFailEvent.new(*(1..7))
+        @event = StatusChangeEvent.new(@test, @file, @inner_event)
+      end
+
+      subject { @event }
+
+      its(:type)  { should == @test }
+      its(:file)  { should == @file }
+      its(:event) { should == @inner_event }
+    end
   end
 end

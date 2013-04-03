a7afde4b2bc9d77268a98c7fb65e53c80c564b01
Jon Cairns <jon@joncairns.com>
2013-04-03 15:50:32 +0100

    Add parsing loop in conductor

--- lib/torkify/conductor.rb
+++ lib/torkify/conductor.rb
@@ -12,6 +12,16 @@ module Torkify
     end
 
     def start
+      parser = EventParser.new
+      @reader.each_line do |line|
+        event = parser.parse line
+        dispatch event
+      end
+    end
+
+    protected
+    def dispatch(event)
+      @observers.each { |o| o.send event.type.to_sym, event }
     end
   end
 end

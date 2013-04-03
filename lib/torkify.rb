16f4a6602c1050844f910b1fd0252f736956b955
Jon <jon@joncairns.com>
2013-04-03 20:25:51 +0100

    Fix conductor spec due to interface changes

--- lib/torkify.rb
+++ lib/torkify.rb
@@ -10,20 +10,26 @@ module Torkify
         puts event.inspect
       end
     end
-  def self.start
-    require 'torkify/reader'
-    require 'torkify/exceptions'
-    require 'torkify/event_parser'
-    require 'torkify/conductor'
-    require 'torkify/test_event'
-    require 'torkify/pass_or_fail_event'
-    require 'torkify/status_change_event'
-    require 'torkify/observer_set'
 
+  def self.start
+    self.load_files
     observers = ObserverSet.new
     observers << Observer.new
     reader = Reader.new
     conductor = Conductor.new reader, observers
     conductor.start
   end
+
+  def self.load_files
+    require 'torkify/reader'
+    require 'torkify/exceptions'
+    require 'torkify/event_parser'
+    require 'torkify/conductor'
+    require 'torkify/observer_set'
+    require 'torkify/events/event'
+    require 'torkify/events/test_event'
+    require 'torkify/events/pass_or_fail_event'
+    require 'torkify/events/status_change_event'
+  end
+
 end
--- spec/conductor_spec.rb
+++ spec/conductor_spec.rb
@@ -4,7 +4,8 @@ module Torkify
   describe Conductor do
     before do
       @reader = double("Torkify::Reader")
-      @conductor = Conductor.new @reader
+      @observers = Torkify::ObserverSet.new
+      @conductor = Conductor.new @reader, @observers
     end
 
     subject { @conductor }
@@ -12,10 +13,12 @@ module Torkify
     it { should respond_to :observers }
     it { should respond_to :start }
 
+    its(:observers) { should equal @observers }
+
     context "when an observer is added" do
       before do
         @observer = Object.new
-        @conductor.add_observer @observer
+        @conductor.observers.add @observer
       end
 
       subject { @conductor.observers.first }
@@ -27,6 +30,26 @@ module Torkify
       it "should call each_line on reader" do
         @reader.should_receive(:each_line)
         @conductor.start
+      end
+
+      # TODO tidy this up
+      context "with dummy input" do
+        before do
+          def @reader.each_line
+            lines = ['["test","spec/status_change_event_spec.rb",[],"spec/status_change_event_spec.rb.log",0]']
+            lines.each { |l| yield l }
+          end
+          @obs1 = double
+          @obs2 = double
+          @observers.add @obs1
+          @observers.add @obs2
+        end
+
+        it "should notify each observer" do
+          @obs1.should_receive(:on_test)
+          @obs2.should_receive(:on_test)
+          @conductor.start
+        end
       end
     end
   end

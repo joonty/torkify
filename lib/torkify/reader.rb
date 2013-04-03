a41327cdb44b3035c56ddc3c578410eff8b2247a
Jon Cairns <jon@joncairns.com>
2013-04-03 15:26:36 +0100

    Tidy up reader class with comments
    Also remove redundant loading of the stringio library.

--- lib/torkify/reader.rb
+++ lib/torkify/reader.rb
@@ -1,8 +1,14 @@
 module Torkify
-  require 'stringio'
   require 'open3'
 
   class Reader
+    # Open the tork command and initialize the streams.
+    #
+    # STDOUT is kept as the underlying stream, and this class can be used as
+    # an IO-like object on STDOUT.
+    #
+    # A TorkError is raised if the command fails, and its message is whatever
+    # has been written to the command's STDERR stream.
     def initialize(command = 'tork-remote tork-engine', run_in_dir = Dir.pwd)
       Dir.chdir(run_in_dir)
 
@@ -13,10 +19,14 @@ module Torkify
       end
     end
 
+    # Pass all unknown methods straight to the underlying IO object.
+    #
+    # This allows this class to be used in an IO like way.
     def method_missing(method, *args, &blk)
       @io.send(method, *args, &blk)
     end
 
+    # Allow respond_to? to work with method_missing.
     def respond_to?(method, include_private = false)
       @io.respond_to? method, include_private
     end

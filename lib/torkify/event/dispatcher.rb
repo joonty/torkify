9765ca56e640471e9ba4fe4949911acbad6c59cc
Jon Cairns <jon@joncairns.com>
2013-06-05 09:46:08 +0100

    Basic event dispatching functionality in Dispatcher class

--- lib/torkify/event/dispatcher.rb
+++ lib/torkify/event/dispatcher.rb
@@ -1,6 +1,14 @@
 module Torkify::Event
   class Dispatcher
     def initialize(observers)
+      @observers = observers
     end
+
+    def dispatch(event)
+      observers.each { |o| o.send event.message, event }
+    end
+
+  protected
+    attr_reader :observers
   end
 end
--- spec/event/dispatcher_spec.rb
+++ spec/event/dispatcher_spec.rb
@@ -1,7 +1,45 @@
 require 'spec_helper'
 require 'torkify/event/dispatcher'
+require 'torkify/event/event'
 
 module Torkify::Event
+
+  class ExampleObserver
+    def on_test(event)
+    end
+
+    def on_pass
+    end
+  end
+
   describe Dispatcher do
+    context "with a single observer" do
+      let(:observer) { mock }
+      subject { Dispatcher.new [observer] }
+
+      context "dispatching an example event" do
+        let(:event) { Event.new :example }
+
+        it "should call on_example on the observer" do
+          observer.should_receive(:on_example).with(event)
+          subject.dispatch event
+        end
+      end
+    end
+
+    context "with multiple observers" do
+      let(:observers) { [].fill mock, 0, 3 }
+      subject { Dispatcher.new observers }
+
+      context "dispatching an absorb event" do
+        let(:event) { Event.new :absorb }
+
+        it "should call on_absorb on the observer" do
+          observers.each { |o| o.should_receive(:on_absorb).with(event) }
+          subject.dispatch event
+        end
+      end
+
+    end
   end
 end
